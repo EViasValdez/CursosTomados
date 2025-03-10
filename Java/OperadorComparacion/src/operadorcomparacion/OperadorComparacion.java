804c7964e48a11e3b615430e70e3b240a27941f9
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-03-10 09:46:38 -0600

    Add files via upload

---
+++ Java/OperadorComparacion/src/operadorcomparacion/OperadorComparacion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorcomparacion;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class OperadorComparacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int A = 20;
+        int B = 20;
+        
+        System.out.println(A == B);
+        System.out.println(A != B);
+        System.out.println(A > B);
+        System.out.println(A < B);
+        System.out.println(A >= B);
+        System.out.println(A <= B);
+    }
+}
